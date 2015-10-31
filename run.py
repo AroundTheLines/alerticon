c87886f52a72d2256724ed4dc2fa5d6601753202
Qile Wang <wangqile123@gmail.com>
2015-10-31 19:13:55 -0400

    Images maybe?

--- run.py
+++ run.py
@@ -1,15 +1,29 @@
 from flask import Flask, request, redirect
 import twilio.twiml
- 
+
 app = Flask(__name__)
- 
+
+#Try adding your own number to this list!
+callers = {
+	"+14158675309": "Curious George",
+	"+14167006502": "Qile",
+}
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
- 
+	#Respond to incoming calls with a simple text message.
+
+	from_number = request.values.get('From',None)
+	if from_number in callers:
+		message = callers[from_number] + ", thanks for the message!"
+	else:
+		message = "Monkey, thanks for the message!"
+
+	resp = twilio.twiml.Response()
+	with resp.message("Hello, Mobile Monkey") as m:
+		m.media("https://demo.twilio.com/owl.png")
+		
+	return str(resp)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
