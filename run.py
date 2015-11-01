bfede8fc37cff5da3e28f61bcad58e6cbf27724f
Qile Wang <wangqile123@gmail.com>
2015-10-31 20:21:30 -0400

    Get a list of useable numbers

--- run.py
+++ run.py
@@ -2,12 +2,26 @@ from flask import Flask
 import twilio.twiml
  
 app = Flask(__name__)
+
+# Try adding your own number to this list!
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14167006502": "Qile",
+}
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
+		# Get the caller's phone number from the incoming Twilio request
+    from_number = request.values.get('From', None)
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    # if the caller is someone we know:
+    if from_number in callers:
+        # Greet the caller by name
+        resp.say("Hello " + callers[from_number])
+    else:
+    		resp.say("Hello Monkey")
  
     return str(resp)
  
