8bd2e7da17dfc0c96cb1bdb5d63ca483c2ac5c5a
Qile Wang <wangqile123@gmail.com>
2015-10-31 20:53:50 -0400

    maybe copypasting will work this time?

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ callers = {
     "+14158675309": "Curious George",
     "+14158675310": "Boots",
     "+14158675311": "Virgil",
-    "+14158675312": "Marcel"
 }
  
 @app.route("/", methods=['GET', 'POST'])
@@ -24,10 +23,11 @@ def hello_monkey():
     # Play an mp3
     resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
  
-    # Say a command, and listen for the caller to press a key. When they press
-    # a key, redirect them to /handle-key.
+    # Gather digits.
     with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
-        g.say("To speak to a real monkey, press 1. Press any other key to start over.")
+        g.say("""To speak to a real monkey, press 1. 
+                 Press 2 to record your own monkey howl.
+                 Press any other key to start over.""")
  
     return str(resp)
  
@@ -35,7 +35,6 @@ def hello_monkey():
 def handle_key():
     """Handle key press from a user."""
  
-    # Get the digit pressed by the user
     digit_pressed = request.values.get('Digits', None)
     if digit_pressed == "1":
         resp = twilio.twiml.Response()
@@ -46,9 +45,27 @@ def handle_key():
  
         return str(resp)
  
+    elif digit_pressed == "2":
+        resp = twilio.twiml.Response()
+        resp.say("Record your monkey howl after the tone.")
+        resp.record(maxLength="30", action="/handle-recording")
+        return str(resp)
+ 
     # If the caller pressed anything but 1, redirect them to the homepage.
     else:
         return redirect("/")
+ 
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+    """Play back the caller's recording."""
+ 
+    recording_url = request.values.get("RecordingUrl", None)
+ 
+    resp = twilio.twiml.Response()
+    resp.say("Thanks for howling... take a listen to what you howled.")
+    resp.play(recording_url)
+    resp.say("Goodbye.")
+    return str(resp)
  
 if __name__ == "__main__":
     app.run(debug=True)
