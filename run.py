f8a7129ffa74af7e76e4a335adefeecd293e31a2
Qile Wang <wangqile123@gmail.com>
2015-10-31 20:24:28 -0400

    Fixed the imports

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
-
+ 
 # Try adding your own number to this list!
 callers = {
     "+14158675309": "Curious George",
@@ -12,16 +12,16 @@ callers = {
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-		# Get the caller's phone number from the incoming Twilio request
+    # Get the caller's phone number from the incoming Twilio request
     from_number = request.values.get('From', None)
-    """Respond to incoming requests."""
     resp = twilio.twiml.Response()
+ 
     # if the caller is someone we know:
     if from_number in callers:
         # Greet the caller by name
         resp.say("Hello " + callers[from_number])
     else:
-    		resp.say("Hello Monkey")
+        resp.say("Hello Monkey")
  
     return str(resp)
  
