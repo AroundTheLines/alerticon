c8bd515e821a0f582b8beee3786c885e5552e7dc
Qile Wang <wangqile123@gmail.com>
2015-10-31 20:41:22 -0400

    okay, that's fine. why do you break.

--- run.py
+++ run.py
@@ -3,29 +3,28 @@ import twilio.twiml
  
 app = Flask(__name__)
  
-# Try adding your own number to this list!
 callers = {
     "+14158675309": "Curious George",
     "+14158675310": "Boots",
-    "+14167006502": "Qile",
+    "+14158675311": "Virgil",
 }
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    # Get the caller's phone number from the incoming Twilio request
-    from_number = request.values.get('From', None)
-    
+    """Say a caller's name, and play an MP3 for them."""
  
-    # if the caller is someone we know:
+    from_number = request.values.get('From', None)
     if from_number in callers:
-        # Greet the caller by name
         caller = callers[from_number]
     else:
         caller = "Monkey"
-
- 		resp = twilio.twiml.Response()
- 		resp.say("Hello " + caller)
- 		resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+ 
+    resp = twilio.twiml.Response()
+    # Greet the caller by name
+    resp.say("Hello " + caller)
+    # Play an MP3
+    resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+ 
     return str(resp)
  
 if __name__ == "__main__":
